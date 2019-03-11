1ffa658bda0a4b91775b305382db4e098ac72e59
Nikita Pedorenko <nikitnya1@gmail.com>
2019-03-11 17:05:40 +0200

    Added Student.FromCsv

--- LAB2OP/LAB2OP/Student.cs
+++ LAB2OP/LAB2OP/Student.cs
@@ -1,4 +1,5 @@
-﻿namespace LAB2OP
+﻿using System;
+namespace LAB2OP
 {
     class Student
     {
@@ -13,6 +14,21 @@
             this.isContract = isContract;
         }
 
-        
+        static Student FromCsv(string line)
+        {
+            string name = line.Substring(0, line.IndexOf(","));
+
+            string[] marksStr = line.Substring(line.IndexOf(",") + 1, line.LastIndexOf(",") - line.IndexOf(",") - 1).Split(',');
+            int[] marks = Array.ConvertAll<string, int>(marksStr, int.Parse);
+
+            string contract = line.Substring(line.LastIndexOf(",") + 1, line.Length - line.LastIndexOf(",") - 1);
+            bool isContract;
+            if (contract == "TRUE" || contract == "+")
+                isContract = true;
+            else
+                isContract = false;
+
+            return new Student(name, marks, isContract);
+        }
     }
 }
